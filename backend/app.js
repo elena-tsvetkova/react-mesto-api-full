fab5f6f27066d6e757ca4ced7c874976660ff13d
Елена <lena.lena1803@yandex.ru>
2022-09-07 17:01:31 +0300

    подключение к базе

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://0.0.0.0:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
